1e64f480e320f04186ee7d266b50f4941debe98c
Avi <avicse509@gmail.com>
2017-10-01 00:29:25 +0530

    database seeder for dumy data

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	// Lets truncate our existing record to start form scratch
+    	Article::truncate();
+
+    	$faker = \Faker\Factory::create();
+
+    	// lets create few article in our Article table
+    	for ($i=0; $i <50 ; $i++) { 
+    		Article::Create([
+    			'title' => $faker->sentence,
+    			'body' => $faker->paragraph,
+    		]);
+    	}
+    }
+}
